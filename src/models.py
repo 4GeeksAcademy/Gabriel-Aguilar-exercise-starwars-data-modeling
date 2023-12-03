2c930f27c5987a512a7038cdaa80602656d5836b
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2023-12-03 13:53:23 +0000

    sql

--- src/models.py
+++ src/models.py
@@ -4,29 +4,80 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    email = Column(Integer, primary_key=True)
+    usuario = Column(String(250), ForeignKey('naves_favoritas'),ForeignKey('personas_favoritas'),ForeignKey('planetas_favoritas'))
+    password = Column(String(250), nullable=False)
+
+
+class Naves(Base):
+    __tablename__ = 'naves'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    modelo = Column(String(250), nullable=False)
+    clase_de_nave = Column(String(250), nullable=False)
+    costo_en_creditos = Column(String(250), nullable=False)
+    longitud = Column(String(250), nullable=False)
+    capacidad_de_pasajeros = Column(Integer, nullable=False)
+    MGLT = Column(String(250), nullable=False)
+    capacidad_de_carga = Column(Integer, nullable=False)
+    provisiones = Column(Integer, nullable=False)
+    hypermotor = Column(String(250), nullable=False)
+class Personas(Base):
+    __tablename__ = 'personas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    genero =Column(String(250), nullable=False)
+    especie =Column(String(250), nullable=False)
+    height =Column(String(250), nullable=False)
+    mass=Column(String(250), nullable=False)
+    skin_color =Column(String(250), nullable=False)
+    eye_color =Column(String(250), nullable=False)
+    birth_year =Column(String(250), nullable=False)
+    hair_color =Column(String(250), nullable=False)
+class Planetas(Base):
+    __tablename__ = 'planetas'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    terreno =Column(String(250), nullable=False)
+    poblacion =Column(String(250), nullable=False)
+    diametro =Column(String(250), nullable=False)
+    rotation_period =Column(String(250), nullable=False)
+    orbital_period =Column(String(250), nullable=False)
+    gravity =Column(String(250), nullable=False)
+    climate =Column(String(250), nullable=False)
+    surface_water =Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Naves_favoritas(Base):
+    __tablename__ = 'naves_favoritas'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nave_id = Column(Integer, ForeignKey('naves.id'))
+    usuario_id =Column(Integer, ForeignKey('naves.id'))
+class Personas_favoritas(Base):
+    __tablename__ = 'personas_favoritas'
+    id = Column(Integer, primary_key=True)
+    nave_id = Column(Integer, ForeignKey('personas.id'))
+    usuario_id =Column(Integer, ForeignKey('personas.id'))
+class Planetas_favoritas(Base):
+    __tablename__ = 'planetas_favoritas'
+    id = Column(Integer, primary_key=True)
+    nave_id = Column(Integer, ForeignKey('planetas.id'))
+    usuario_id =Column(Integer, ForeignKey('planetas.id'))
+
+ 
+    
+
 
     def to_dict(self):
         return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
